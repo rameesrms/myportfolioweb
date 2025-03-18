cdf05bf89894a3a51258bb1cf36627d9f354a1d8
admin <rameesrms000@gmail.com>
2025-03-18 10:53:28 +0530

    home animations

---
+++ lib/view/screens/data.dart
@@ -0,0 +1,33 @@
+import 'package:mywebapp/view/screens/top-section.dart';
+
+List<MyProjectModel> myProjects = [
+MyProjectModel(name: "EDUMARTIAL",
+  image: "assets/images/logo_edumartial.png",
+  description: "Management of a sports school by providing a comprehensive app that caters to the needs of schools, coaches, and students. App offers specialized logins for each user type and tracks the activities and sports performance of students, aiming to enhance the overall efficiency and effectiveness of sports education.",
+availableAt:[
+  AvailablePlatform(platform:"playstore",url:"https://play.google.com/store/apps/details?id=com.edumartial.softfruits.edumartial&pcampaignid=web_share"),
+  AvailablePlatform(platform:"appstore",url:"https://apps.apple.com/in/app/edumartial/id6572308144")
+
+],
+),
+MyProjectModel(name: "Finprov",description: "Management of a sports school by providing a comprehensive app that caters to the needs of schools, coaches, and students. App offers specialized logins for each user type and tracks the activities and sports performance of students, aiming to enhance the overall efficiency and effectiveness of sports education.",
+availableAt:[
+  AvailablePlatform (platform:"playstore",url:"assets/images/logo_finprov.jpg"),
+  AvailablePlatform (platform:"appstore",url:"assets/images/logo_finprov.jpg")
+],image: "assets/images/logo_finprov.jpg"
+),MyProjectModel(name: "Kabayan",description: "Management of a sports school by providing a comprehensive app that caters to the needs of schools, coaches, and students. App offers specialized logins for each user type and tracks the activities and sports performance of students, aiming to enhance the overall efficiency and effectiveness of sports education.",
+availableAt:[
+  AvailablePlatform(platform:"playstore",url:"assets/images/logo_kabayan.png"),
+  AvailablePlatform(platform:"appstore",url:"assets/images/logo_kabayan.png"),
+],image: "assets/images/logo_kabayan.png"
+),MyProjectModel(name: "DoctorOnQ",description: "Management of a sports school by providing a comprehensive app that caters to the needs of schools, coaches, and students. App offers specialized logins for each user type and tracks the activities and sports performance of students, aiming to enhance the overall efficiency and effectiveness of sports education.",
+      availableAt:[
+        AvailablePlatform(platform:"playstore",url:"assets/images/logo_dq_app.png")
+      ],image: "assets/images/logo_dq_app.png"
+  ),
+  MyProjectModel(name: "elora",description: "Management of a sports school by providing a comprehensive app that caters to the needs of schools, coaches, and students. App offers specialized logins for each user type and tracks the activities and sports performance of students, aiming to enhance the overall efficiency and effectiveness of sports education.",
+      availableAt:[
+        AvailablePlatform(platform:"playstore",url:"assets/images/logo_elora.png")
+      ],image: "assets/images/logo_elora.png"
+  )
+];
